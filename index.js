66a38b54a07dab82661be2624f0906f00ff5e4a8
milneoa15 <milneoa15@outlook.com>
2025-03-23 15:44:09 +1300

    Implement reset button

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 
-function onRoll (dicer) {
-    const randomNumber = Math.floor(Math.random() * 6) + 1;
+function displayWinner (p1, p2) {
+    var title = document.querySelector("h1");
 
+    if (p1 == 0) {
+        title.innerHTML = "Dice Simulator";
+    } else if (p1 > p2) {
+        title.innerHTML = "Player 1 Wins!";
+    } else if (p2 > p1) {
+        title.innerHTML = "Player 2 Wins!";
+    } else {
+        title.innerHTML = "It's a Draw!";
+    }
+}
+
+function changeDice (dicer, randomNumber) {
     dicer.innerHTML = "";
 
     for (var i=0; i<randomNumber; i++) {
@@ -12,7 +24,24 @@ function onRoll (dicer) {
     dicer.classList.add("dice-"+randomNumber);
 }
 
+function onRoll (dicer) {
+    const randomNumber = Math.floor(Math.random() * 6) + 1;
+
+    changeDice (dicer, randomNumber);
+
+    return randomNumber;
+}
+
 function onRoller () {
-    onRoll (document.querySelector(".p1"));
-    onRoll (document.querySelector(".p2"));
+    p1 = onRoll (document.querySelector(".p1"));
+    p2 = onRoll (document.querySelector(".p2"));
+
+    displayWinner (p1, p2);
+}
+
+function resetPage () {
+    changeDice (document.querySelector(".p1"), 1);
+    changeDice (document.querySelector(".p2"), 1);
+
+    displayWinner (0, 0);
 }
